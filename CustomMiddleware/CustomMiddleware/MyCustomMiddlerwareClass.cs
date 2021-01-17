d8313df31e09fb357c41a19924663f0303b57c7f
giriparveen <giriparveen@gmail.com>
2021-01-17 20:03:56 +0530

    Initial commit

---
+++ CustomMiddleware/CustomMiddleware/MyCustomMiddlerwareClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleware.CustomMiddleware
+{
+    
+    public class MyCustomMiddlerwareClass
+    {
+        RequestDelegate _next;
+        public MyCustomMiddlerwareClass(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync("I am from custom middler ware");
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class UseMyCustomMiddlewareClass
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder builder)
+        {
+          return  builder.UseMiddleware<MyCustomMiddlerwareClass>();
+        }
+    }
+}
